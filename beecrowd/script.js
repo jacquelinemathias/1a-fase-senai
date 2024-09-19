e02634bac051c13a41f9524b28e903058f6a9dd8
Jacqueline Mathias <jacqueline.mathias38@gmail.com>
2024-09-18 22:15:43 -0300

    Exercício 1038
    Adição do exercício 1038

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -1,3 +1,30 @@
+// ta errado
+function executar1038(){
+    let pedido, codigoLanche, qtdLanche, pagamento
+    while(codigoLanche!=0){
+    codigoLanche = Number(prompt("Qual lanche você deseja comprar? (Digite 0 para encerrar seleção)\n1- Cachorro quente R$4,00\n2-X-Salada R$4,50\n3-X-Bacon R$5,00\n4- Torrada Simples R$2,00\n5- Refrigerante R$1,50"));
+    qtdLanche = Number(prompt("Quantos você deseja comprar?"));
+    if(codigoLanche==1){
+        pagamento = qtdLanche * 4
+        alert("O valor do seu lanche é R$"+pagamento);
+    }else if(codigoLanche==2){
+        pagamento = qtdLanche * 4.50
+        alert("O valor do seu lanche é R$"+pagamento);
+    }else if(codigoLanche==3){
+        pagamento = qtdLanche * 5
+        alert("O valor do seu lanche é R$"+pagamento);
+    }else if(codigoLanche==4){
+        pagamento = qtdLanche * 2
+        alert("O valor do seu lanche é R$"+pagamento);
+    }else if(codigoLanche==5){
+        pagamento = qtdLanche * 1.50
+        alert("O valor do seu lanche é R$"+pagamento);
+    }else{
+        alert("Até a próxima!");
+    }
+    }
+}
+
 function executar1009(){
     let nome, salarioFixo, vendas, salarioTotal
     nome = prompt("Informe seu nome:");
@@ -15,3 +42,4 @@ function executar1008(){
     salario = horas * valorHora
     alert("O funcionário "+id+" receberá R$"+salario.toFixed(2)+" no próximo pagamento.");
 }
+
